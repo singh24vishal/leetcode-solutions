5b0fa9ed58bb373ea6bb6e513dd2a73975782bfb
singh24vishal <119792586+singh24vishal@users.noreply.github.com>
2023-01-03 15:22:09 +0530

    Time: 321 ms (48.83%), Space: 65.3 MB (68.40%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -1,31 +1,12 @@
 class Solution {
 public:
-    bool fun(vector<int>&v,int n,int mx)
-    {
-        if(v[1]-v[0]>mx or v[n-1]-v[n-2]>mx) return false;
-        for(int i=2;i<n;i+=2)
-        {
-            if(v[i]-v[i-2]>mx) return false;
-        }
-        for(int i=3;i<n;i+=2)
-        {
-            if(v[i]-v[i-2]>mx) return false;
-        }
-        return true;
-    }
     int maxJump(vector<int>& v) {
-        int l=0,h=1e9;
-        int ans=INT_MAX,n=v.size();
-        while(l<=h)
-        {
-            int mid=(l+h)/2;
-            if(fun(v,n,mid))
-            {
-                ans=mid;
-                h=mid-1;
-            }
-            else l=mid+1;
-        }
+        int n=v.size();
+        int ans=-1;
+        ans=max(ans,v[1]-v[0]);
+        ans=max(ans,v[n-1]-v[n-2]);
+        for(int i=2;i<n;i++)
+        ans=max(ans,v[i]-v[i-2]);
         return ans;
     }
 };
