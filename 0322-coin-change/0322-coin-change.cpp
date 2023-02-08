c5a0f52016e8f228c3ea62fc0817e8360eb247e2
singh24vishal <119792586+singh24vishal@users.noreply.github.com>
2023-02-08 11:42:12 +0530

    Time: 70 ms (91.69%), Space: 10.5 MB (90.79%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int sum) {
+        int n=coins.size();
+        int t[n+1][sum+1];
+        for(int i=0;i<=sum;i++)
+            t[0][i]=INT_MAX-1;
+        for(int i=1;i<=n;i++)
+            t[i][0]=0;
+        // for(int i=1;i<=sum;i++)
+        // {
+        //     if(i%coins[0]==0)
+        //         t[1][i]=i/coins[0];
+        //     else t[1][i]=INT_MAX-1;
+        // }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=sum;j++)
+            {
+                if(coins[i-1]<=j)
+                    t[i][j]=min(1+t[i][j-coins[i-1]],t[i-1][j]);
+                else t[i][j]=t[i-1][j];
+            }
+        }
+        if(t[n][sum]>sum)
+            return -1;
+        return t[n][sum];
+    }
+};
