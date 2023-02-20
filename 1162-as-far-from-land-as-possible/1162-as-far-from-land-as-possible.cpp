3bef1065658340b1a07c3862009d3678df76b245
singh24vishal <119792586+singh24vishal@users.noreply.github.com>
2023-02-20 10:06:06 +0530

    Time: 68 ms (69.71%), Space: 19.9 MB (60.88%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
+    bool ok(int i,int j,int n,int m)
+    {
+        return (i>=0 and i<n and j>=0 and j<m);
+    }
     int maxDistance(vector<vector<int>>& v) {
         int n=v.size(),m=v[0].size();
-        vector<pair<int,int>> a,b;
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(v[i][j]==0) b.push_back({i,j});
-                else a.push_back({i,j});
+                if(v[i][j]==1) q.push({i,j});
             }
         }
+        if(q.size()==0 or q.size()==n*m) return -1;
         int ans=-1;
-        for(auto [x,y]:b)
+        vector<pair<int,int>> vp={{1,0},{-1,0},{0,1},{0,-1}};
+        while(q.size())
         {
-            int temp=INT_MAX;
-            for(auto [i,j]:a)
-                temp=min(temp,abs(i-x)+abs(j-y));
-            if(temp!=INT_MAX)
-                ans=max(ans,temp);
+            ans++;
+            int temp=q.size();
+            for(int i=0;i<temp;i++)
+            {
+                auto [x,y]=q.front();
+                q.pop();
+                for(auto [a,b]:vp)
+                {
+                    int c=x+a,d=y+b;
+                    if(ok(c,d,n,m) and v[c][d]==0)
+                    {
+                        q.push({c,d});
+                        v[c][d]=1;
+                    }
+                }
+            }
         }
         return ans;
     }
