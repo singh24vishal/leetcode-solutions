c04c660b3d14b14c02d27e936b3a43cba26c58b1
singh24vishal <119792586+singh24vishal@users.noreply.github.com>
2023-03-13 12:14:31 +0530

    Time: 69 ms (92.65%), Space: 46.6 MB (8.77%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -5,15 +5,14 @@ public:
     {
         return (i>=0 and j>=0 and i<n and j<m);
     }
-    void dfs(int i,int j,int n,int m,vector<vector<int>>&v,int &c)
+    void dfs(int i,int j,int n,int m,vector<vector<int>>&v)
     {
-        c++;
         v[i][j]=0;
         for(auto [x,y]:vp)
         {
             int a=i+x,b=j+y;
             if(ok(a,b,n,m) and v[a][b]!=0)
-                dfs(a,b,n,m,v,c);
+                dfs(a,b,n,m,v);
         }
     }
     int numEnclaves(vector<vector<int>>& v) {
@@ -21,21 +20,18 @@ public:
         int c=0;
         for(int i=0;i<n;i++)
         {
-            if(v[i][0]==1) dfs(i,0,n,m,v,c);
-            if(v[i][m-1]==1) dfs(i,m-1,n,m,v,c);
+            if(v[i][0]==1) dfs(i,0,n,m,v);
+            if(v[i][m-1]==1) dfs(i,m-1,n,m,v);
         }
         for(int i=0;i<m;i++)
         {
-            if(v[0][i]==1) dfs(0,i,n,m,v,c);
-            if(v[n-1][i]==1) dfs(n-1,i,n,m,v,c);
+            if(v[0][i]==1) dfs(0,i,n,m,v);
+            if(v[n-1][i]==1) dfs(n-1,i,n,m,v);
         }
         c=0;
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
-            {
-                if(v[i][j]==1) dfs(i,j,n,m,v,c);
-            }
+            for(int j=0;j<m;j++) c+=v[i][j];
         }
         return c;
     }
