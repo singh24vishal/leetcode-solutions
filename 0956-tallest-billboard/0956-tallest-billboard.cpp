a3cea7dd1cc7cc548f4515c3713fa295da299e10
singh24vishal <119792586+singh24vishal@users.noreply.github.com>
2023-03-20 16:31:14 +0530

    Time: 321 ms (29.13%), Space: 64.4 MB (33.98%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.cpp
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int fun(int i,vector<int>&rods,int diff,vector<unordered_map<int,int>>&dp,int n)
-    {
-        if(i==n)
-        {
-            if(diff==0) return 0;
-            return INT_MIN;
-        }
-        if(dp[i].find(diff)!=dp[i].end()) return dp[i][diff];
-        int a=fun(i+1,rods,diff,dp,n);
-        int b=rods[i]+fun(i+1,rods,diff+rods[i],dp,n);
-        int c=fun(i+1,rods,diff-rods[i],dp,n);
-        return dp[i][diff]=max({a,b,c});
-    }
     int tallestBillboard(vector<int>& rods) {
-        int n=rods.size();
-        vector<unordered_map<int,int>> dp(n);
-        return fun(0,rods,0,dp,n);
+        unordered_map<int,int> dp;
+        dp[0]=0;
+        for(auto e:rods)
+        {
+            unordered_map<int,int> cur=dp;
+            for(auto it:cur)
+            {
+                int d=it.first;
+                dp[d+e]=max(dp[d+e],cur[d]);
+                dp[abs(d-e)]=max(dp[abs(d-e)],cur[d]+min(d,e));
+            }
+        }
+        return dp[0];
     }
 };
