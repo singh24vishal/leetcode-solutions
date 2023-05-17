d055b36483b18a464bc6b542afb564e896916973
singh24vishal <119792586+singh24vishal@users.noreply.github.com>
2023-05-17 11:52:02 +0530

    Time: 50 ms (11.74%), Space: 16.5 MB (24.13%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int fun(int i,int j,vector<int>&v,vector<vector<int>>&dp)
+    int fun(int i,int j,vector<vector<int>> &dp,int n,vector<int> &v,int c)
     {
-        if(i>j) return 0;
-        if(dp[i][j]) return dp[i][j];
-        int x=0,y=0;
-        x=v[i]+min(fun(i+2,j,v,dp),fun(i+1,j-1,v,dp));
-        y=v[j]+min(fun(i+1,j-1,v,dp),fun(i,j-2,v,dp));
-        return dp[i][j]=max(x,y);
+        if(i>=n or j<0 or i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int a=fun(i+1,j,dp,n,v,1-c);
+        int b=fun(i,j-1,dp,n,v,1-c);
+        int x1=v[i]+a;
+        int x2=v[j]+b;
+        if(c==0) dp[i][j]=max(x1,x2);
+        else dp[i][j]=min(a,b);
+        return dp[i][j];
     }
     bool stoneGame(vector<int>& v) {
         int n=v.size();
-        int i=0,j=n-1;
         int sum=accumulate(v.begin(),v.end(),0);
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        fun(i,j,v,dp);
-        return dp[0][n-1]*2>sum;
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+		int ans=fun(0,n-1,dp,n,v,0);
+        return 2*ans>sum;
     }
 };
